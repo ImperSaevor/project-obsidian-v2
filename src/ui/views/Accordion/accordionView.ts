6313e9f902ed4043002addedfeccc174febc24e1
STILOGSUD\leherpeux <alexandre.leherpeux@stilog.com>
2025-10-10 10:55:28 +0200

    Change accordion view

--- src/ui/views/Accordion/accordionView.ts
+++ src/ui/views/Accordion/accordionView.ts
@@ -1,7 +1,6 @@
 ﻿// src/ui/views/Accordion/accordionView.ts
-import { ProjectView, type DataQueryResult, type ProjectViewProps } from "src/customViewApi";
+import { ProjectView, type ProjectViewProps } from "src/customViewApi";
 import AccordionViewSvelte from "./AccordionView.svelte";
-import type { AccordionConfig } from "./types";
 
 export class AccordionView extends ProjectView<any> {
     view?: AccordionViewSvelte | null;
@@ -11,11 +10,15 @@ export class AccordionView extends ProjectView<any> {
     getIcon() { return "chevrons-down-up"; }
 
     // Accepte DataFrame OU { data: DataFrame }
-    async onData({ data }: DataQueryResult) {
-        this.view?.$set({ frame: data });
+    async onData(arg: any) {
+        const df = arg?.data ? arg.data : arg;           // compat V2/V3
+        const records = Array.isArray(df?.records) ? [...df.records] : [];
+        // Pousser une nouvelle référence évite l’égalité stricte et déclenche l’update Svelte
+        this.view?.$set({ frame: { records } });
+        console.log("[Accordion] records:", records.length);
     }
 
-    async onOpen(props: ProjectViewProps<AccordionConfig>) {
+    async onOpen(props: ProjectViewProps<any>) {
         this.view = new AccordionViewSvelte({
             target: props.contentEl,
             props: {
@@ -26,12 +29,11 @@ export class AccordionView extends ProjectView<any> {
                 config: props.config,
                 onConfigChange: props.saveConfig,
                 getRecordColor: props.getRecordColor,
-            },
+                sortRecords: props.sortRecords,
+                getRecord: props.getRecord,
+            }
         });
     }
 
-    async onClose() {
-        this.view?.$destroy();
-        this.view = null;
-    }
+    async onClose() { this.view?.$destroy(); this.view = null; }
 }
